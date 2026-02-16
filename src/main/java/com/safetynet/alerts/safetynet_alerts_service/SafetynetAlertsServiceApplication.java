e41116357f64a68ceef1a3e64ffc7f6e66aa6328
Jhon-Stiven <jh.virtual.tech@gmail.com>
2026-02-16 18:55:25 +0100

    Initial Spring Boot SafetyNet project

---
+++ src/main/java/com/safetynet/alerts/safetynet_alerts_service/SafetynetAlertsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts.safetynet_alerts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/alerts/safetynet_alerts_service/SafetynetAlertsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts.safetynet_alerts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
